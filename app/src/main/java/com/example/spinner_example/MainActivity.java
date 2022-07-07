004760052313d5b40434a3c0c135383117df5d5f
KotrotsKon <kotrotskon@users.noreply.github.com>
2022-07-07 19:23:37 +0300

    implements AdapterView.OnItemSelectedListener

--- app/src/main/java/com/example/spinner_example/MainActivity.java
+++ app/src/main/java/com/example/spinner_example/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.spinner_example;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
 
         String[] planets = getResources().getStringArray(R.array.planets_array);
 
@@ -21,6 +26,17 @@ public class MainActivity extends AppCompatActivity {
 
         spinner.setAdapter(adapter);
 
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        TextView textView = (TextView) view;
+        Toast.makeText(this, textView.getText().toString() , Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
